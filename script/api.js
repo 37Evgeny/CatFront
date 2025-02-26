7d55aef8474e362b55a5920e8e1b76224798ea10
37Evgeny <pysskuu88eicloud.com>
2025-02-26 19:00:22 +0300

    добавил коменты поправил код

--- script/api.js
+++ script/api.js
@@ -1,90 +1,3 @@
-// const CONFIG_API = {
-//     url: 'http://89.111.169.234:3000',
-//     headers: {
-//         'Content-type': 'application/json'
-//     }
-// }
-
-// class Api {
-//     constructor(config){
-//         this._url = config.url;
-//         this._headers = config.headers;
-//     }
-
-//      // Метод  
-//      _onRes(response){
-//         // const cats = await response.json(); // Извлекаем данные
-        
-//         return response.ok ? response.json() : Promise.reject({...response, message: "Server side error!!!"})
-       
-//     }
-
-//     // Получаем всех котиков с сервера
-//      getAllCats(){
-//         // return вернуть цепочку промисов
-//        return fetch(`${this._url}/cats`, {
-//             method: 'GET'
-//         }).then(this._onRes)  
-       
-//     }
-
-
-
-// // // Добавить котика на сервер
-// //     addNewCat(data){
-// //         return fetch(`${this._url}/add`, {
-// //             method: 'POST',
-// //             body: JSON.stringify(data),
-// //             headers: this._headers
-// //         }).then(this._onRes)
-// //     }
-// }
-
-// const api = new Api(CONFIG_API);
-
-// const CONFIG_API = {
-//     url: 'http://89.111.169.234:3000',
-//     headers: {
-//         'Content-Type': 'application/json' // учтите, что заглавные буквы могут иметь значение, в зависимости от сервера
-//     }
-// };
-
-// class Api {
-//     constructor(config) {
-//         this._url = config.url;
-//         this._headers = config.headers;
-//     }
-
-//     // Метод для обработки ответа
-//     _onRes(response) {
-//         if (!response.ok) {
-//             // Если статус ответа не в диапазоне 200-299, возвращаем ошибку
-//             return Promise.reject({ ...response, message: "Server side error!!!" });
-//         }
-//         console.log(response.json())
-//         return response.json(); // Возвращаем данные в формате JSON
-//     }
-
-//     // Получаем всех котиков с сервера
-//     getAllCats() {
-//         // Возвращаем цепочку промисов
-//          return fetch(`${this._url}/cats`, {
-//             method: 'GET',
-//             headers: this._headers // добавляем заголовки
-//         }).then(this._onRes)
-//     }
-
-//     // Добавить котика на сервер
-//     addNewCat(data) {
-//         return fetch(`${this._url}/add`, {
-//             method: 'POST',
-//             body: JSON.stringify(data),
-//             headers: this._headers
-//         }).then(this._onRes);
-//     }
-// }
-
-// const api = new Api(CONFIG_API);
 
 const CONFIG_API = {
     url: 'http://89.111.169.234:3000',
@@ -126,14 +39,25 @@ class Api {
         }).then(this._onRes)
     }
 
-    // Метод для удаления котика по ID
-deleteCat(catId) {
+    // удаления котика по ID
+    deleteCat(catId) {
     return fetch(`${this._url}/cats/${catId}`, {
         method: 'DELETE',
         headers: this._headers
     })
     .then(this._onRes)
 }
+
+  // Установка удаление лайkа
+  changeLikeCat(catId, isLike) {
+    return fetch(`${this._url}/cats/likes/${catId}`,{
+        method: isLike? "DELETE" : "PUT",
+        headers: this._headers 
+    } ).then(onResponce)
+    // .catch((err) => { console.log(`ошибка ${err}`) })
+}
+
+
 }
 
 const api = new Api(CONFIG_API);
--- script/index.js
+++ script/index.js
@@ -1,19 +1,25 @@
+// Контейнер для карточек с котиками
 const cardsContainer = document.querySelector('.cards');
-const btnOpenPopup= document.querySelector('#add');
-const btnOpenAuthorization= document.querySelector('#login');
+// Кнопка для открытия всплывающего окна добавления котика
+const btnOpenPopup = document.querySelector('#add');
+ // Кнопка для открытия формы авторизации
+const btnOpenAuthorization = document.querySelector('#login');
+// Форма для добавления нового котика
 const formCatAdd = document.querySelector('#popup-form-cat');
+// Форма авторизации (не используется в коде)
 const formAuthorization = document.querySelector('#form-login');
 
 // вызов метода getAllCats получакм всех котиков
 api.getAllCats()
     .then(data => {
-        console.log(data + 'index'); // Логируем данные о котиках
+        // Логируем данные о котиках
+        console.log(data + 'index'); 
         data.forEach(function(dataCat){
                         // Создаем карточу
                         console.log(dataCat)
                         const cardInst = new Card(dataCat, '#card-template');
                         // Возвращаем разметку
-                        const newCardElem= cardInst.getElement();
+                        const newCardElem = cardInst.getElement();
                         // Добавляем на страницу  
                         cardsContainer.append(newCardElem);
                     }) 
@@ -25,55 +31,53 @@ api.getAllCats()
 
 // Функция добавления котика на страницу
 function formAddCat(event){
+    // Предотвращаем стандартное поведение формы
     event.preventDefault();
+    // Получаем элементы формы
     const elementsFormCat = [...formCatAdd.elements];
+    // Сериализуем данные формы
     const dataFromForm = serializeForm(elementsFormCat);
     // добавление котика на сервер
     api.addNewCat(dataFromForm)
         .then(()=>{
+            // Создаем карточку для нового котика
             const cardInst = new Card(dataFromForm, '#card-template');
-            const newCardElem= cardInst.getElement();
+            // Получаем разметку карточки
+            const newCardElem = cardInst.getElement();
+            // Добавляем карточку на страницу
             cardsContainer.append(newCardElem);
             //  закрытие popup
             AddCat.close();
         })
 }
 
+// Функция для сериализации данных формы
 function serializeForm(elem){
     const formData ={};
     elem.forEach(input =>{
+        // Игнорируем кнопку отправки
         if(input.type === 'submit') return;
         if(input.type !== 'checkbox') {
+            // Сохраняем значение текстовых полей
             formData[input.name]= input.value;
         }
         if(input.type === 'checkbox') {
+            // Сохраняем состояние чекбоксов
             formData[input.name]= input.checked;
         }
     })
+    // Возвращаем объект с данными формы
     return formData;
 }
 
-
-// Функция добавления котика на страницу
-function formAddCat(event){
-    event.preventDefault();
-    const elementsFormCat = [...formCatAdd.elements];
-    const dataFromForm = serializeForm(elementsFormCat);
-    // добавление котика на сервер
-    api.addNewCat(dataFromForm)
-        .then(()=>{
-            const cardInst = new Card(dataFromForm, '#card-template');
-            const newCardElem= cardInst.getElement();
-            cardsContainer.append(newCardElem);
-            //  закрытие popup
-            AddCat.close();
-        })
-}
-
-// Вызов модельного окна для добавления котика
+// Создаем экземпляр всплывающего окна для добавления котика
 const AddCat = new Popup('popup-add-cats')
+// Устанавливаем обработчики событий для всплывающего окна
 AddCat.setEventListener()
+// Устанавливаем обработчики событий для кнопок
+// Открываем всплывающее окно при нажатии на кнопку
 btnOpenPopup.addEventListener('click',()=>  AddCat.open());
+// Обрабатываем отправку формы добавления котика
 formCatAdd.addEventListener('submit', formAddCat)
 
 // //Функция Авторизации
